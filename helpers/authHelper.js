eef6953b980d834e96dd666ff71859e6b91dbe0f
rohit chauhan <chauhanrc7878@gmail.com>
2026-06-25 23:39:13 +0530

    client

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -4,6 +4,7 @@ export const hashPassword = async(password) =>{
     try{
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds)
+        console.log(hashedPassword);
         return hashedPassword 
     }catch(error){
         console.log(error)
