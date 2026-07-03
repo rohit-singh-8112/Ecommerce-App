4f6dc9b9cd9c80a25abbc82935d2ddf968598bf8
rohit chauhan <chauhanrc7878@gmail.com>
2026-07-03 23:47:37 +0530

    return

---
+++ client/src/pages/user/Return.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../component/layout/Layout'
+const Return = () => {
+  return (
+    <Layout>
+      <div>Return</div>
+    </Layout>
+  )
+}
+
+export default Return
